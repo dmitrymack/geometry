a00cb9c7d7a317f748434f37696d230fd0a7cb05
dmitrymack <dmitrymack12@gmail.com>
2019-03-03 20:18:00 +0700

    Created and filled dinamic arrays

--- geometry.c
+++ geometry.c
@@ -3,11 +3,19 @@
 
 int main()
 {
-    int n;
+    int n, *arr_x, *arr_y, *arr_rad;
     printf("Input the number of circles -> ");
     scanf("%d", &n);
+    arr_x = malloc(n * sizeof(int));
+    arr_y = malloc(n * sizeof(int));
+    arr_rad = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
+        printf("Input the x, y and radius of %d circle -> ", i + 1);
+        scanf("%d%d%d", &arr_x[i], &arr_y[i], &arr_rad[i]);
     }
 
+    free(arr_x);
+    free(arr_y);
+    free(arr_rad);
     return 0;
 }
