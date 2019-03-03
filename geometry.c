095f5c5f3c5cd6491a29d25f71c50713e3ca17ba
dmitrymack <dmitrymack12@gmail.com>
2019-03-03 12:10:11 +0700

    Added input the number of circles

---
+++ geometry.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Input the number of circles -> ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+    }
+
+    return 0;
+}
